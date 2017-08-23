35f3fc67d7715b89c0be5aa7a1d0e26044cb4b63
armin <armin@192.168.0.206>
2017-08-23 08:24:56 +0200

    Added logging to internal functions.

--- src/device_acces.py
+++ src/device_acces.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Armin Koefler, file created at 19.08.2017
 # Helper code for regulator<i>.pi
 # The present code allows checking for other processes that are currently interacting 
@@ -5,19 +7,24 @@
 
 import fcntl
 
+# lock file definition
 mDeviceLock = "/var/lock/regulator.lock"
 
 # Tries to lock the device. Boolean return value describes result.
 def tryLockDevice():
-        lockfile = open(mDeviceLock, 'w')
-        try:
-                fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
-                return True
-        except Exception as e:
-                return False
+    lockfile = open(mDeviceLock, 'w')       
+    try:
+        fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        print("Acquired device lock successfully")
+        return True
+    except:
+        print("Acquired device lock successfully")
+        return False
 
-# Unlocks the device that previously has been locked by the current process. No error check.
+# Unlocks the device that previously has been locked by the current process. 
+# No error check upon previous lock acquisition.
 def unlockDevice():
         lockfile = open(mDeviceLock, 'w')
         fcntl.lockf(lockfile, fcntl.LOCK_UN)
+        print("Unlocked device")
         
