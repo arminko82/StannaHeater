a30af7437923437a798367ef7f9420f6c2e359f5
armin <armin@192.168.0.206>
2017-08-25 08:05:00 +0200

    Finalized (?) entry file, constants were moved before, main handler core rewritten

--- src/regulator.py
+++ src/regulator.py
@@ -6,24 +6,18 @@
 # the rotor by a certain amount (degree would be nice).
 # Current finding: 512 steps equal a full rotation so that one degree is actually 0.703 degrees
 
-from device import *
-from device_acces import tryLockDevice, unlockDevice
-from Carbon.AppleEvents import kAEAutoDown
+import sys
+import signal
+from const_vars import mSetupDone
+from device_access import tryLockDevice, unlockDevice
+from logic import doCalibrate, doGetAngle, doTurn
 
-# Block of response codes for external callers
-RESULT_OK = 0
-ERROR_COMMON = 1
-ERROR_DEVICE_IN_USE = 2
-
-# The file holds the currenty known angle. It is reset to 0 when doCalibrate is called which
-# moves the rotor to the lef-most location and rewrites this file. Any call to doTurn updates the 
-# file's value.
-ANGLE_FILE = "./current_angle.dat"
-
+# Event handler which is run when the current process ends based on a SIGINT.
+# Cleans up.
 def shutdown(ignore1, ignore2):
     resetGpioState()
-    sys.exit(0)
 
+# Prints the how to use text to console. Optionally shows an error information in the first line.
 def printDescription(error):
     print("""{0}
              Call this using one of the following arguments:
@@ -41,76 +35,41 @@ def printDescription(error):
     
 # Parses command and executes. Returns error code or success or an angle if in command mode -getAngle
 def main():
-    cmd = ""
-    argc = len(sys.argv)
-    if argc == 1:
-        printDescription("Error: No command line arguments")
-    elif argc == 2:
-        cmd = sys.argv[1]
-        if cmd == "-calibrate":
-            return doCalibrate()
-        elif cmd == "-getAngle":
-            return doGetAngle()
-        else:
-            printDescription("Command '{0}' not known".format(cmd))
-    elif argc == 3:
-        cmd = sys.argv[1]
-        if cmd == "-turn":
-            return doTurn(argv[2])
-        else:
-            printDescription("Command '{0}' not known".format(cmd))
-    else:
-            printDescription("Command '{0}' not known or invalid number of arguments".format(cmd))
-    return ERROR_COMMON
-        
-def doCalibrate():
-    try:
-        with open(ANGLE_FILE, "w") as file:
-            file.write("0")
-        # todo turn to the left till 0 position before file writing
-        raise Exception("No handling for implemented yet, no hardware created.")
-        return RESULT_OK
-    except Exception as ex:
-        print(ex)
-        return ERROR_COMMON
-            
-def doGetAngle():
-    try:
-        with open(ANGLE_FILE) as file:
-            return float(file.readline()) # culture invariant?
-    except:
-        return ERROR_COMMON
-    
-def doTurn(angleString):
-    try:
-        angle = float(angleString)
-    except:
-        printDescription("Angle was not a floating point number")
-        return ERROR_COMMON
-    
+    global mSetupDone
+    mSetupDone = False
     if tryLockDevice() == False:
         print("Another process is accessing the device at the moment.")
         return ERROR_DEVICE_IN_USE
-
     try:
-        setup()
         signal.signal(signal.SIGINT, shutdown)
-        print("Waiting for a second ...")
-        resetGpioState()
-        time.sleep(1)
-        GPIO.output(enable_pin, 1)
-        print("Setting up ...")
-
-        while True:
-            delay = raw_input("Zeitverzoegerung (ms)?")
-            steps = raw_input("Wie viele Schritte vorwaerts? ")
-            forward(int(delay) / 1000.0, int(steps))
-            steps = raw_input("Wie viele Schritte rueckwaerts? ")
-            backwards(int(delay) / 1000.0, int(steps))
-        return RESULT_OK
+        cmd = ""
+        argc = len(sys.argv)
+        if argc == 1:
+            printDescription("Error: No command line arguments")
+        elif argc == 2:
+            cmd = sys.argv[1]
+            if cmd == "-calibrate":
+                return doCalibrate()
+            elif cmd == "-getAngle":
+                return doGetAngle()
+            else:
+                printDescription("Command '{0}' not known".format(cmd))
+        elif argc == 3:
+            cmd = sys.argv[1]
+            if cmd == "-turn":
+                return doTurn(argv[2])
+            else:
+                printDescription("Command '{0}' not known".format(cmd))
+        else:
+                printDescription("Command '{0}' not known or invalid number of arguments".format(cmd))
+        return ERROR_COMMON
+    except Exception as x:
+        print("Error: " + x)
     finally:
+        if mSetupDone:
+            resetGpioState()
         unlockDevice()
-
+        
+# Some strange entry point found in this language
 if __name__ == "__main__":
     main()
-
