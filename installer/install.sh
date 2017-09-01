c56bf68f2a63dce441b6335c7643a85797909068
armin <armin@192.168.0.206>
2017-09-01 09:09:42 +0200

    Installer creates common log folder /var/log/StannaHeater

--- installer/install.sh
+++ installer/install.sh
@@ -13,13 +13,18 @@ apt-get -y install php5-common php5-cgi php5
 mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.original
 cp lighttpd.conf /etc/lighttpd/lighttpd.conf 
 
-#5 Setup server group and user
+#4 Setup server group and user
 groupadd www-regulator
 useradd -g www-regulator www-regulator
 
-#4 Deploy website
+#5 create log folders
+mkdir /var/log/StannaHeater
+mkdir /var/log/StannaHeater
+chown -R www-regulator:www-regulator /var/log/StannaHeater
+
+#6 Deploy website
 mkdir /var/www/regulator
 cp -r ../www/* /var/www/regulator/
 chown -R www-regulator:www-regulator /var/www/regulator
 
-#5 Register HTTP server to auto-start 
+#7 Register HTTP server to auto-start 
