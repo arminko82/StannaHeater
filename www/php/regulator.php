377b0dbca7241cbd649a0684fd0824889b769d43
armin <arminko82@gmail.com>
2017-09-17 12:31:50 +0200

    Tidy php, provide better logging - still no proper repsonse

--- www/php/regulator.php
+++ www/php/regulator.php
@@ -1,16 +1,37 @@
 <?php
+define("LOG_FILE", "out.log");
 // Return values from controller script
-define("RESULT_OK", 0);
-define("ERROR_COMMON", 1);
-define("ERROR_DEVICE_IN_USE", 2);
-define("ERROR_UNKNOWN_FUNCTION", 3);
-define("ERROR_ANGLE", -361);
+define("RESULT_OK", '0');
+define("ERROR_COMMON", '1');
+define("ERROR_DEVICE_IN_USE", '2');
+define("ERROR_UNKNOWN_FUNCTION", '3');
+define("ERROR_ANGLE", '-361');
 
-// A logger that writes to stdout while in debug mode
-function log1($msg) 
+// Kind of init code in global space of this file's scope.
+// This is used to determind the function the ajax query wanted to call
+// and passes the given parameters to that function.
+if (isset($_POST['execTurn']))
 {
-    echo "PHP: " . $msg;    
+    log1("Executing isset1");
+    return execTurn($_POST['execTurn']);
 }
+elseif (isset($_POST['execCalibrate']))
+{
+    log1("Executing isset2");
+    return execCalibrate();
+}
+elseif (isset($_POST['execGetAngle']))
+{
+    log1("Executing isset3");
+    return execGetAngle();
+}
+else
+{
+    log1("Executing isset4");
+    return ERROR_UNKNOWN_FUNCTION;
+}
+// end "init code"
+
 // Forwards the call to the backend controller.
 // Tries to turn the lever to the desired end point.
 function execTurn($desiredAngle)
@@ -38,47 +59,32 @@ function execGetAngle()
     return $output;
 }
 
-// Kind of init code in global space of this file's scope.
-// This is used to determind the function the ajax query wanted to call
-// and passes the given parameters to that function.
-if (isset($_POST['execTurn']))
-{
-    log1("Executing isset1");
-    return execTurn($_POST['execTurn']);
-} 
-elseif (isset($_POST['execCalibrate']))
-{
-    log1("Executing isset2");
-    return execCalibrate();
-} 
-elseif (isset($_POST['execGetAngle']))
-{
-    log1("Executing isset3");
-    return execGetAngle();
-}
-else
-{
-    log1("Executing isset4");
-    return ERROR_UNKNOWN_FUNCTION;
-}
-// end "init code"
-
 // Interprets the received return values for the python scripts.
+// Writes the response code to stdout which becomes part of the HTTP response
+// on HTTP POST handling
 // @return bool|null
 function evaluateOutput($output)
 {
+    $out = bin2hex($output);
+    log1("evaluateOutput: " . $out);
+    echo strval($output); // actual HTTP response output writer
     switch ($output) {
         case RESULT_OK:
-            //echo "TODO Notify ok";
             return true;
-        case ERROR_COMMON:
-            //echo "TODO Notify on some error";
-            return false;
         case ERROR_DEVICE_IN_USE:
-            //echo "TODO Notfiy on hardware in use";
+        case ERROR_COMMON:
             return false;
         default:
             return null;
     }
+}
+
+// A logger that writes to stdout while in debug mode
+function log1($msg)
+{
+    date_default_timezone_set('Europe/Vienna');
+    $now = date("Y-m-d H:i:s");
+    $line = sprintf("%s => %s\r\n", $now, $msg);
+    file_put_contents(LOG_FILE, $line, FILE_APPEND);
 }
 ?>
